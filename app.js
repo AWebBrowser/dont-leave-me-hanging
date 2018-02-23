2a99e47966cb8a9b7b810dc5ac053817164d0ba4
AWebBrowser <gabrielito0604@gmail.com>
2018-02-22 16:22:10 -1000

    2/22/18

---
+++ app.js
@@ -0,0 +1,13 @@
+var alphabet =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var words = ["hi", "bye", "alphabet", "green", "blue", "apple", "circle", "orange"];
+var guess = "";
+var contents =[];
+function startGame() {
+	var random = Math.floor(Math.random());
+	var wordToGuess = words[random];
+	guess = guessWord;
+	for () {
+		document.getElementById('letters-container').innerHTML = alphabet;
+	}
+	contents.push(document.getElementById('word-container'));
+}
